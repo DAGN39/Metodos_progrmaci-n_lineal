867fb833671fd1e9a9b283a0f0e44df0a669f3ef
DAGN39 <168099862+DAGN39@users.noreply.github.com>
2025-12-05 17:22:30 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -346,5 +346,6 @@ def resolver():
             "tipo": tipo
         }), 400
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render usa PORT
+    app.run(host="0.0.0.0", port=port)
